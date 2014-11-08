47ca2738b71925eadd05d8d376bfa84a86ac2097
Håkon Åmdal <hakon@aamdal.com>
2014-11-07 19:28:58 -0600

    tete

--- wander.cpp
+++ wander.cpp
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
 
 
   // turn on the motors, turn off amigobot sounds
-  /*robot.enableMotors();
+  robot.enableMotors();
   robot.comInt(ArCommands::SOUNDTOG, 0);
 
   // add a set of actions that combine together to effect the wander behavior
@@ -111,7 +111,6 @@ int main(int argc, char **argv)
   
   // wait for robot task loop to end before exiting the program
   robot.waitForRunExit();
-  */
   
   Aria::exit(0);
   puts("Here?");
