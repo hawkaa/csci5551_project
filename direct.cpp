19de1eeb7052ded63857f597fe6bee93f1bd21aa
Håkon Åmdal <hakon@aamdal.com>
2014-11-07 20:56:29 -0600

    tete

--- direct.cpp
+++ direct.cpp
@@ -110,6 +110,7 @@ int main(int argc, char **argv)
   Aria::init();
   
   ArArgumentParser argParser(&argc, argv);
+  argParser.addDefaultArgument("-rp /dev/ttyUSB0");
   argParser.loadDefaultArguments();
 
 
